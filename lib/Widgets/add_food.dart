8b637bf9883ae55a498c9cd3dea3bd6fb2b9545d
mobileapp234 <mobileapprm234@gmail.com>
2023-05-24 09:29:31 +0300

    delete

---
+++ lib/Widgets/add_food.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AddFood extends StatefulWidget {
+  const AddFood({super.key});
+
+  @override
+  State<AddFood> createState() => _AddFoodState();
+}
+
+class _AddFoodState extends State<AddFood> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: FloatingActionButton(
+        onPressed: () {},
+      )),
+    );
+  }
+}

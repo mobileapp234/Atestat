1f6f9fe5a8c8fe7756001e8b62a5c4df7aa38f9e
Robert <robert.valentino.ciobanu@gmail.com>
2023-05-26 20:43:40 +0300

    a mers!
    Co-authored-by: Marian-05 <Marian-05@users.noreply.github.com>

--- lib/Widgets/choose_food_type.dart
+++ lib/Widgets/choose_food_type.dart
@@ -1,223 +1,223 @@
-import 'package:flutter/material.dart';
-import 'package:mobile_app/Globals_Variables.dart' as c;
+// import 'package:flutter/material.dart';
+// import 'package:mobile_app/Globals_Variables.dart' as c;
 
-class ChooseFoodType extends StatefulWidget {
-  const ChooseFoodType({super.key});
+// class ChooseFoodType extends StatefulWidget {
+//   const ChooseFoodType({super.key});
 
-  @override
-  State<ChooseFoodType> createState() => _ChooseFoodTypeState();
-}
+//   @override
+//   State<ChooseFoodType> createState() => _ChooseFoodTypeState();
+// }
 
-class _ChooseFoodTypeState extends State<ChooseFoodType> {
-  @override
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-        width: double.infinity,
-        height: 100,
-        child: ListView(
-          padding: EdgeInsets.zero,
-          primary: false,
-          shrinkWrap: true,
-          scrollDirection: Axis.horizontal,
-          children: [
-            GestureDetector(
-              onTap: () async {
-                setState(() {
-                  c.categorie = 0;
-                  // print(c.categorie);
-                });
-                // dispose();
-              },
-              child: Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(16, 8, 0, 8),
-                child: Container(
-                  width: 120,
-                  height: 190,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    boxShadow: const [
-                      BoxShadow(
-                        blurRadius: 4,
-                        color: Color(0x230E151B),
-                        offset: Offset(0, 2),
-                      )
-                    ],
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 4),
-                          child: Icon(
-                            Icons.soup_kitchen_sharp,
-                            color: Color(0xFF57636C),
-                            size: 44,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
-                          child: Text('Ciorbe', style: TextStyle(fontSize: 12)),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () async {
-                setState(() {
-                  c.categorie = 1;
+// class _ChooseFoodTypeState extends State<ChooseFoodType> {
+//   @override
+//   @override
+//   Widget build(BuildContext context) {
+//     return SizedBox(
+//         width: double.infinity,
+//         height: 100,
+//         child: ListView(
+//           padding: EdgeInsets.zero,
+//           primary: false,
+//           shrinkWrap: true,
+//           scrollDirection: Axis.horizontal,
+//           children: [
+//             GestureDetector(
+//               onTap: () async {
+//                 setState(() {
+//                   c.categorie = 0;
+//                   // print(c.categorie);
+//                 });
+//                 // dispose();
+//               },
+//               child: Padding(
+//                 padding: const EdgeInsetsDirectional.fromSTEB(16, 8, 0, 8),
+//                 child: Container(
+//                   width: 120,
+//                   height: 190,
+//                   decoration: BoxDecoration(
+//                     color: Colors.white,
+//                     boxShadow: const [
+//                       BoxShadow(
+//                         blurRadius: 4,
+//                         color: Color(0x230E151B),
+//                         offset: Offset(0, 2),
+//                       )
+//                     ],
+//                     borderRadius: BorderRadius.circular(12),
+//                   ),
+//                   child: const Padding(
+//                     padding: EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
+//                     child: Column(
+//                       mainAxisSize: MainAxisSize.max,
+//                       mainAxisAlignment: MainAxisAlignment.center,
+//                       crossAxisAlignment: CrossAxisAlignment.center,
+//                       children: [
+//                         Padding(
+//                           padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 4),
+//                           child: Icon(
+//                             Icons.soup_kitchen_sharp,
+//                             color: Color(0xFF57636C),
+//                             size: 44,
+//                           ),
+//                         ),
+//                         Padding(
+//                           padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+//                           child: Text('Ciorbe', style: TextStyle(fontSize: 12)),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             GestureDetector(
+//               onTap: () async {
+//                 setState(() {
+//                   c.categorie = 1;
 
-                  /// print(c.categorie);
-                });
-              },
-              child: Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(12, 8, 0, 8),
-                child: Container(
-                  width: 120,
-                  height: 190,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    boxShadow: const [
-                      BoxShadow(
-                        blurRadius: 4,
-                        color: Color(0x230E151B),
-                        offset: Offset(0, 2),
-                      )
-                    ],
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 4),
-                          child: Icon(
-                            Icons.food_bank_outlined,
-                            color: Color(0xFF57636C),
-                            size: 44,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
-                          child:
-                              Text('Felul 2', style: TextStyle(fontSize: 12)),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                  c.categorie = 2;
-                  print(c.categorie);
-                });
-              },
-              child: Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(12, 8, 0, 8),
-                child: Container(
-                  width: 120,
-                  height: 190,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    boxShadow: const [
-                      BoxShadow(
-                        blurRadius: 4,
-                        color: Color(0x230E151B),
-                        offset: Offset(0, 2),
-                      )
-                    ],
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 4),
-                          child: Icon(
-                            Icons.cake_outlined,
-                            color: Color(0xFF57636C),
-                            size: 44,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
-                          child: Text('Desert', style: TextStyle(fontSize: 12)),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                  c.categorie = 3;
-                  print(c.categorie);
-                });
-              },
-              child: Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(12, 8, 0, 8),
-                child: Container(
-                  width: 120,
-                  height: 190,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    boxShadow: const [
-                      BoxShadow(
-                        blurRadius: 4,
-                        color: Color(0x230E151B),
-                        offset: Offset(0, 2),
-                      )
-                    ],
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 4),
-                          child: Icon(
-                            Icons.wine_bar_outlined,
-                            color: Color(0xFF57636C),
-                            size: 44,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
-                          child:
-                              Text('Bauturi', style: TextStyle(fontSize: 12)),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ));
-  }
-}
+//                   /// print(c.categorie);
+//                 });
+//               },
+//               child: Padding(
+//                 padding: const EdgeInsetsDirectional.fromSTEB(12, 8, 0, 8),
+//                 child: Container(
+//                   width: 120,
+//                   height: 190,
+//                   decoration: BoxDecoration(
+//                     color: Colors.white,
+//                     boxShadow: const [
+//                       BoxShadow(
+//                         blurRadius: 4,
+//                         color: Color(0x230E151B),
+//                         offset: Offset(0, 2),
+//                       )
+//                     ],
+//                     borderRadius: BorderRadius.circular(12),
+//                   ),
+//                   child: const Padding(
+//                     padding: EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
+//                     child: Column(
+//                       mainAxisSize: MainAxisSize.max,
+//                       mainAxisAlignment: MainAxisAlignment.center,
+//                       crossAxisAlignment: CrossAxisAlignment.center,
+//                       children: [
+//                         Padding(
+//                           padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 4),
+//                           child: Icon(
+//                             Icons.food_bank_outlined,
+//                             color: Color(0xFF57636C),
+//                             size: 44,
+//                           ),
+//                         ),
+//                         Padding(
+//                           padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+//                           child:
+//                               Text('Felul 2', style: TextStyle(fontSize: 12)),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             GestureDetector(
+//               onTap: () {
+//                 setState(() {
+//                   c.categorie = 2;
+//                   print(c.categorie);
+//                 });
+//               },
+//               child: Padding(
+//                 padding: const EdgeInsetsDirectional.fromSTEB(12, 8, 0, 8),
+//                 child: Container(
+//                   width: 120,
+//                   height: 190,
+//                   decoration: BoxDecoration(
+//                     color: Colors.white,
+//                     boxShadow: const [
+//                       BoxShadow(
+//                         blurRadius: 4,
+//                         color: Color(0x230E151B),
+//                         offset: Offset(0, 2),
+//                       )
+//                     ],
+//                     borderRadius: BorderRadius.circular(12),
+//                   ),
+//                   child: const Padding(
+//                     padding: EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
+//                     child: Column(
+//                       mainAxisSize: MainAxisSize.max,
+//                       mainAxisAlignment: MainAxisAlignment.center,
+//                       crossAxisAlignment: CrossAxisAlignment.center,
+//                       children: [
+//                         Padding(
+//                           padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 4),
+//                           child: Icon(
+//                             Icons.cake_outlined,
+//                             color: Color(0xFF57636C),
+//                             size: 44,
+//                           ),
+//                         ),
+//                         Padding(
+//                           padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+//                           child: Text('Desert', style: TextStyle(fontSize: 12)),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             GestureDetector(
+//               onTap: () {
+//                 setState(() {
+//                   c.categorie = 3;
+//                   print(c.categorie);
+//                 });
+//               },
+//               child: Padding(
+//                 padding: const EdgeInsetsDirectional.fromSTEB(12, 8, 0, 8),
+//                 child: Container(
+//                   width: 120,
+//                   height: 190,
+//                   decoration: BoxDecoration(
+//                     color: Colors.white,
+//                     boxShadow: const [
+//                       BoxShadow(
+//                         blurRadius: 4,
+//                         color: Color(0x230E151B),
+//                         offset: Offset(0, 2),
+//                       )
+//                     ],
+//                     borderRadius: BorderRadius.circular(12),
+//                   ),
+//                   child: const Padding(
+//                     padding: EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
+//                     child: Column(
+//                       mainAxisSize: MainAxisSize.max,
+//                       mainAxisAlignment: MainAxisAlignment.center,
+//                       crossAxisAlignment: CrossAxisAlignment.center,
+//                       children: [
+//                         Padding(
+//                           padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 4),
+//                           child: Icon(
+//                             Icons.wine_bar_outlined,
+//                             color: Color(0xFF57636C),
+//                             size: 44,
+//                           ),
+//                         ),
+//                         Padding(
+//                           padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+//                           child:
+//                               Text('Bauturi', style: TextStyle(fontSize: 12)),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ));
+//   }
+// }
